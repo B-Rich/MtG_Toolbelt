e9e2a333dacd52b83567ff997698329a732c9e7b
bedfordwest <bedfordwest@gmail.com>
2014-08-24 02:53:19 -0500

    +/- buttons now adjust life totals.

--- app/src/main/java/com/bedfordwest/bedford/mtgtoolbelt/HomeScreen.java
+++ app/src/main/java/com/bedfordwest/bedford/mtgtoolbelt/HomeScreen.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class HomeScreen extends Activity {
@@ -32,5 +34,36 @@ public class HomeScreen extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //Change the displayed life totals when the +/- buttons are pressed
+    public void adjustLife(View v){
+
+        //Get the current life totals from the EditText views and convert them to integers
+        final EditText yourlifelabel = (EditText)findViewById(R.id.your_life);
+        final EditText opponentlifelabel = (EditText)findViewById(R.id.opponent_life);
+        int yourlife = Integer.parseInt(yourlifelabel.getText().toString());
+        int opponentlife = Integer.parseInt(opponentlifelabel.getText().toString());
+
+        //Determine which button was pressed
+        switch (v.getId()) {
+            case(R.id.plus_your_life):
+                yourlife++;
+                yourlifelabel.setText(String.valueOf(yourlife));
+                break;
+            case(R.id.minus_your_life):
+                yourlife--;
+                yourlifelabel.setText((String.valueOf(yourlife)));
+                break;
+            case(R.id.plus_opponent_life):
+                opponentlife++;
+                opponentlifelabel.setText(String.valueOf(opponentlife));
+                break;
+            case(R.id.minus_opponent_life):
+                opponentlife--;
+                opponentlifelabel.setText((String.valueOf(opponentlife)));
+                break;
+
+        }
     }
 }
